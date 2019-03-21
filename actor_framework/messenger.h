79be4f194f033354c15c5a453d519a1e66e4de03
nikosxenakis <s1770309@ed.ac.uk>
2019-03-21 20:58:41 +0000

    documentation added

--- actor_framework/messenger.h
+++ actor_framework/messenger.h
@@ -5,13 +5,44 @@
 #include <assert.h>
 #include "message.h"
 
+/**
+ * @class Messenger
+ * @details class that provides functionality to send messages over MPI
+ */
 class Messenger {
 
-public:
+private:
+	/**
+	 * @brief MPI_Datatype describing the message type
+	 */
 	static MPI_Datatype Message_type;
+
+public:
+
+	/**
+	 * @brief initialises the MPI_Datatype
+	 */
 	static void init_types();
-	static void send_blocking_message(int worker_pid, Message message);
+
+	/**
+	 * @brief sends an ansyncronous buffered message
+	 * @param worker_pid the destination process
+	 * @param the message data to send
+	 */
 	static void send_message(int worker_pid, Message message);
+
+	/**
+	 * @brief sends an blocking message
+	 * @param worker_pid the destination process
+	 * @param the message data to send
+	 */
+	static void send_blocking_message(int worker_pid, Message message);
+
+	/**
+	 * @brief receives a message
+	 * @param source_pid the source process
+	 * @return Returns the received message
+	 */
 	static Message receive_message(int source_pid);
 };
 
